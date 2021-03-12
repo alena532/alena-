d5354c3b94433dc2f7213cced7a527c06d4efec8
alena532 <79169496+alena532@users.noreply.github.com>
2021-03-12 10:54:20 +0300

    Add files via upload

---
+++ лаба2/replace/l23/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace replace
+{
+    class Program
+    {
+        static bool check(char a)
+        {
+            if(a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int[] asci= new int[s.Length] ;
+            char[] temp = new char[s.Length];
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (!(s[i] >= 'a' && s[i] <= 'z'))
+                {
+                    Console.WriteLine("Mistake!");
+                    System.Environment.Exit (2);
+                }
+            }
+            for (int i = s.Length-1; i >=0; i--)
+            {
+                if (i == 0)
+                {
+                    asci[i] = Convert.ToInt16(s[i]);
+                    break;
+                }
+                if (check(s[i-1]) )
+                {
+                    if (s[i] == 'z')
+                    {
+                        asci[i]= Convert.ToInt16('a');
+                    }
+                    else
+                    {
+                        asci[i] = Convert.ToInt16(s[i]) + 1;
+                    }
+                }
+                else
+                {
+                    asci[i] = Convert.ToInt16(s[i]);
+                }
+            }
+            for(int i = 0; i < asci.Length; i++)
+            {
+                temp[i] = Convert.ToChar(asci[i]);
+            }
+            Console.WriteLine(temp);
+        }
+    }
+}
---
+++ лаба2/reverse/reverse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] another = s.Split(' ');
+            Array.Reverse(another);
+            for (int i = 0; i < another.Length; i++)
+            {
+                Console.Write(another[i]);
+                Console.Write(" ");
+            }
+        }
+    }
+}
