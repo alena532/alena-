bdc3f2ef0fda435091048a54afbf6e0fd3d35f78
alena532 <79169496+alena532@users.noreply.github.com>
2021-04-01 21:45:38 +0300

    Add files via upload

---
+++ vehicle/vehicle/listvehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace vehicle
+{
+    class listvehicle
+    {
+        private List<Vehicle> automobile;
+        private int size = 0;
+        public listvehicle()
+        {
+            automobile = new List<Vehicle>();
+        }
+        public void add(Vehicle machine)
+        {
+            automobile.Add(machine);
+            ++size;
+        }
+        public Vehicle this[int index]
+        {
+            get
+            {
+                return automobile[index];
+            }
+            set
+            {
+                automobile[index] = value;
+            }
+        }
+        public int Size()
+        {
+            return size;
+        }
+
+    }
+
+}
