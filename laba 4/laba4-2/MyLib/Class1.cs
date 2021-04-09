f715396837a3c34fba1f02bacd6c755d8e51012d
alena532 <79169496+alena532@users.noreply.github.com>
2021-04-09 13:09:38 +0300

    Lab5

---
+++ laba 4/laba4-2/MyLib/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public static class Class1
+    {
+        [DllImport("MathLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Add(int a, int b);
+        [DllImport("MathLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Subtrac(int a, int b);
+        [DllImport("MathLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Multiply(int a, int b);
+        [DllImport("MathLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Divide(int a, int b);
+    }
+}
---
+++ laba 4/laba4-2/laba4-2/2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace laba4_2
+{
+    class Program
+    {
+        [DllImport("MyLib.dll",CallingConvention=CallingConvention.Cdecl) ]
+        public static extern int Add(int a, int b);
+        [DllImport("MyLib.dll")]
+        public static extern int Supply(int a, int b);
+        [DllImport("MyLib.dll")]
+        public static extern int Subtection(int a, int b);
+        [DllImport("MyLib.dll")]
+        public static extern int Division(int a, int b);
+        static void Main(string[] args)
+        {
+            int a, b;
+            while (true)
+            {
+                do
+                {
+                    Console.WriteLine("Enter first number");
+                } while (!Int32.TryParse(Console.ReadLine(),out a));
+                do
+                {
+                    Console.WriteLine("Enter second number");
+                } while (!Int32.TryParse(Console.ReadLine(),out b));
+                Console.WriteLine("1.Addition");
+                Console.WriteLine("2.Supply");
+                Console.WriteLine("3.Subtrection");
+                Console.WriteLine("4.Division");
+                Console.WriteLine("5.Finish program");
+                int number;
+                do
+                {
+                    Console.WriteLine("Choose number:");
+                } while (!Int32.TryParse(Console.ReadLine(), out number)&& number>=1 && number<=5 );
+                switch (number)
+                {
+                    case 1:
+                       Console.WriteLine( MyLib.Class1.Add(a, b));
+                        break;
+                    case 2:
+                        Console.WriteLine(MyLib.Class1.Multiply(a, b));
+                        break;
+                    case 3:
+                       Console.WriteLine( MyLib.Class1.Subtrac(a, b));
+                        break;
+                    case 4:
+                       Console.WriteLine( MyLib.Class1.Divide(a, b));
+                        break;
+                    case 5:
+                        return;
+
+
+                        
+                }
+
+            }
+        }
+    }
+}
