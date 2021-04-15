49f7d17bc8dccd95ab06e88c4ab8e6f22135559c
alena532 <79169496+alena532@users.noreply.github.com>
2021-04-15 23:04:08 +0300

    Add files via upload

---
+++ laba3568/laba3568/listvehicle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+namespace vehicle
+{
+    class listvehicle
+    {
+        private List<Vehicle> automobile;
+        private int size;
+
+        int Index;
+        public listvehicle()
+        {
+            Index = -1;
+            automobile = new List<Vehicle>();
+            size = 0;
+        }
+        public void add(Vehicle machine)
+        {
+            automobile.Add(machine);
+            size++;
+        }
+
+        public Vehicle this[int index]
+        {
+            get
+            {
+                return automobile[index];
+            }
+            set
+            {
+                automobile[index] = value;
+            }
+        }
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+            private set
+            {
+                size = value;
+            }
+        }
+
+    }
+
+}
